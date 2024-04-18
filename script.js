9e968af06ec6e35ff2d85b618937fa9a1b3912f0
BADBOYBEN18 <chuama2016@gmail.com>
2024-04-18 15:58:34 +0100

    A weather app built with html,css and javascript with a free api

---
+++ script.js
@@ -0,0 +1,42 @@
+let search = document.getElementById('search');
+let searchBtn = document.getElementById('search-btn');
+let temp = document.getElementById('temp');
+let city = document.getElementById('location');
+let humidity = document.getElementById('humidity');
+let wind = document.getElementById('wind');
+let weatherIcon = document.querySelectorAll('.weather-icon');
+
+let request = new XMLHttpRequest();
+
+searchBtn.addEventListener('click', function () {
+  setInterval(() => {
+    request.open(
+      'GET',
+      `http://api.weatherapi.com/v1/current.json?key=189a35ffbb994c5483d142448241204&q=${search.value}&aqi=yes`
+    );
+
+    request.onerror = function () {
+      alert('Could not find weather');
+    };
+
+    request.onload = () => {
+      // console.log(request);
+      // console.log(request.responseText);
+
+      let myData = request.responseText;
+      // console.log(myData);
+
+      let newData = JSON.parse(myData);
+      console.log(newData);
+
+      city.innerHTML = newData.location.name;
+
+      temp.innerHTML = newData.current.temp_c + '°c';
+
+      humidity.innerHTML = newData.current.humidity + '%';
+      wind.innerHTML = newData.current.wind_kph + 'kph';
+    };
+
+    request.send();
+  }, 3000);
+});
